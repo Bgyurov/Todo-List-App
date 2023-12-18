827963f42187e5f791d620e80a1e5b685a8bc213
Bgyurov <49366221+Bgyurov@users.noreply.github.com>
2023-12-18 16:09:28 +0200

    Manage state , and add loading functionality

--- todoList/App.js
+++ todoList/App.js
@@ -1,20 +1,72 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Keyboard, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View ,ActivityIndicator
+} from 'react-native';
 import React from 'react';
 import Task from './components/Task/Task.js';
 export default function App() {
+
+  const [task, setTask] = React.useState()
+  const [taskItems, setTaskItems] = React.useState([])
+  const [isLoading, setIsLoading] = React.useState(true);
+  const handleAddTask = () => {
+    Keyboard.dismiss()
+    setTaskItems([...taskItems,task])
+    setTask(null)
+  }
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems]  
+    itemsCopy.splice(index,1)
+    setTaskItems(itemsCopy)
+  }
+
+  React.useEffect(() => {
+    const loadData = async () => {
+    
+      await new Promise(resolve => setTimeout(resolve, 2500));
+
+      setIsLoading(false);
+    };
+
+    loadData();
+  }, []);
   return (
     <View style={styles.container}>
-     
-     <View style={styles.tasksWrapper}>
-      <Text style={styles.sectionTitle}>Today`s tasks</Text>
-      <View style={styles.items}>
-        <Task text={'Text 1'}/>
-      </View>
-     </View>
+      {isLoading ? (
+        <View style={styles.spinner}>
+          <Text style={styles.loadingTitle}>Оrganize your day </Text>
+          <ActivityIndicator size="large"  color="#55BCF6"/>
+        </View>
+      ) : (
+        <View style={styles.tasksWrapper}>
+          <Text style={styles.sectionTitle}>Today`s tasks</Text>
+          <View style={styles.items}>
+            {taskItems.map((name, index) => (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={name} />
+              </TouchableOpacity>
+            ))}
+          </View>
+        </View>
+      )}
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+        style={styles.writeTaskWrapper}>
+        <TextInput
+          style={styles.input}
+          placeholder="Write a task"
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
-
+  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -33,4 +85,41 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 30,
   },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#fff',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+     borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  spinner: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  loadingTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginBottom: 15
+  }
 });
